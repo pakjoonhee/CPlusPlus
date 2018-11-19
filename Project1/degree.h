b9cafd8d79fcbddde520f217ebe0c5ce00be7480
Joonhee <pakjoonhee90@gmail.com>
2018-11-18 17:35:44 -0800

    working

--- Project1/degree.h
+++ Project1/degree.h
@@ -15,7 +15,7 @@ protected:
 	DegreeType type;
 
 public:
-	const static int priceArraySize = 3;
+	const static int dayArraySize = 3;
 	Degree();
 	Degree(string studentID, string firstName, string lastName, string emailAddress, int age, 
 		int days[], DegreeType type);
@@ -23,7 +23,7 @@ public:
 	string getStudentID();
 	string getFirstName();
 	string getLastName();
-	string getEmailAddresses();
+	string getEmailAddress();
 	int getAge();
 	int* getDays();
 	virtual DegreeType getDegreeType() = 0;
@@ -31,7 +31,7 @@ public:
 	void setStudentID(string studentID);
 	void setFirstName(string firstName);
 	void setLastName(string lastName);
-	void setEmailAddresses(string emailAddress);
+	void setEmailAddress(string emailAddress);
 	void getAge(int age);
 	void setDays(int days[]);
 	virtual void setDegreeType(DegreeType b) = 0;
--- Project1/repository.cpp
+++ Project1/repository.cpp
@@ -16,7 +16,68 @@ Repository::Repository(int capacity) {
 }
 
 void Repository::parseAdd(string row) {
-	
+	if (lastIndex < capacity) {
+		lastIndex++;
+		double parray[Degree::priceArraySize];
+		if (row[0] == 'F')
+		{
+			this->degrees[lastIndex] = new FictionBook();
+			degrees[lastIndex]->setBookType(FICTION);
+		}
+		else if (row[0] == 'N') {
+			this->degrees[lastIndex] = new NonfictionBook();
+			degrees[lastIndex]->setBookType(NONFICTION);
+		}
+		else
+		{
+			cerr << "INVALID BOOK TYPE!  EXITING NOW!\n";
+			exit(-1);
+		}
+
+		//We will parse through each 'book string' delimiting on the COMMA
+		//We will then extract each subtring and set each field
+		//Set the ID
+		int rhs = studentData[lastIndex].find(",");
+		//cout << studentData[lastIndex].substr(0, rhs) << ' ';
+		degrees[lastIndex]->setID(studentData[lastIndex].substr(0, rhs));
+
+		//read title
+		int lhs = rhs + 1;
+		rhs = studentData[lastIndex].find(",", lhs);
+		//cout << studentData[lastIndex].substr(lhs, rhs - lhs) << ' ';
+		degrees[lastIndex]->setTitle(studentData[lastIndex].substr(lhs, rhs - lhs));
+
+		//read author
+		lhs = rhs + 1;
+		rhs = studentData[lastIndex].find(",", lhs);
+		//cout << studentData[lastIndex].substr(lhs, rhs - lhs) << ' ';
+		degrees[lastIndex]->setAuthor(studentData[lastIndex].substr(lhs, rhs - lhs));
+
+		//read each price
+		//NOTE THAT EACH PRICE MUST BE CONVERTED TO A DOUBLE
+		lhs = rhs + 1;
+		rhs = studentData[lastIndex].find(",", lhs);
+		//cout << studentData[lastIndex].substr(lhs, rhs - lhs) << ' ';
+		parray[0] = stod(studentData[lastIndex].substr(lhs, rhs - lhs));
+
+		lhs = rhs + 1;
+		rhs = studentData[lastIndex].find(",", lhs);
+		//cout << studentData[lastIndex].substr(lhs, rhs - lhs) << ' ';
+		parray[1] = stod(studentData[lastIndex].substr(lhs, rhs - lhs));
+
+		lhs = rhs + 1;
+		rhs = studentData[lastIndex].find(",", lhs);
+		//cout << studentData[lastIndex].substr(lhs, rhs - lhs) << ' ';
+		parray[2] = stod(studentData[lastIndex].substr(lhs, rhs - lhs));
+
+		//set the prices
+		degrees[lastIndex]->setPrices(parray);
+	}
+	else
+	{
+		cerr << "ERROR! LIST HAS EXCEEDED MAXIMUM CAPACITY!\n EXITING NOW!";
+		exit(-1);
+	}
 }
 
 int main() {
